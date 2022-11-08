735eb0c19a91e3f0495d7d48e3652a7fce8ce736
sunliang98 <1700011430@pku.edu.cn>
2022-11-08 20:33:50 +0800

    <Feat> Now train.cpp can read in the input file named `nnINPUT`

--- source/module_esolver/ml-of/train.cpp
+++ source/module_esolver/ml-of/train.cpp
@@ -1,48 +1,252 @@
 #include "./train.h"
 #include <sstream>
 
-Train::Train(int nrxx, int ninpt, int nbatch)
-{
-    this->nrxx = nrxx;
-    this->ninpt = ninpt;
-    this->nbatch = nbatch;
-
-    this->nn = std::make_shared<NN_OFImpl>(this->nrxx, this->ninpt);
-}
 
 Train::~Train()
 {
+    delete[] this->train_cell;
+    delete[] this->train_a;
+    delete[] this->validation_cell;
+    delete[] this->validation_a;
 }
 
-// void Train::setPara(int nrxx, int ninpt, int nbatch)
-// {
-//     this->nrxx = nrxx;
-//     this->ninpt = ninpt;
-//     this->nbatch = nbatch;
-// }
+void Train::readInput()
+{
+    std::ifstream ifs("nnINPUT", std::ios::in);
+    if (!ifs)
+    {
+        std::cout << " Can't find the nnINPUT file." << std::endl;
+        exit(0);
+    }
+
+    char word[80];
+    int ierr = 0;
+
+    ifs.rdstate();
+    while (ifs.good())
+    {
+        ifs >> word;
+        if (ifs.eof()) break;
+
+        if (strcmp("fftdim", word) == 0)
+        {
+            this->read_value(ifs, this->fftdim);
+        }
+        else if (strcmp("nbatch", word) == 0)
+        {
+            this->read_value(ifs, this->nbatch);
+        }
+        else if (strcmp("ntrain", word) == 0)
+        {
+            this->read_value(ifs, this->ntrain);
+            this->train_cell = new std::string[this->ntrain];
+            this->train_a = new double[this->ntrain];
+        }
+        else if (strcmp("nvalidation", word) == 0)
+        {
+            this->read_value(ifs, this->nvalidation);
+            if (this->nvalidation > 0)
+            {
+                this->validation_cell = new std::string[this->nvalidation];
+                this->validation_a = new double[this->nvalidation];
+            }
+        }
+        else if (strcmp("train_dir", word) == 0)
+        {
+            this->read_value(ifs, this->train_dir);
+        }
+        else if (strcmp("train_cell", word) == 0)
+        {
+            for (int i = 0; i < this->ntrain; ++i)
+            {
+                ifs >> this->train_cell[i];
+            }
+        }
+        else if (strcmp("train_a", word) == 0)
+        {
+            for (int i = 0; i < this->ntrain; ++i)
+            {
+                ifs >> this->train_a[i];
+            }
+        }
+        else if (strcmp("validation_dir", word) == 0)
+        {
+            this->read_value(ifs, this->validation_dir);
+        }
+        else if (strcmp("validation_cell", word) == 0 && this->nvalidation > 0)
+        {
+            for (int i = 0; i < this->ntrain; ++i)
+            {
+                ifs >> this->validation_cell[i];
+            }
+        }
+        else if (strcmp("validation_a", word) == 0 && this->nvalidation > 0)
+        {
+            for (int i = 0; i < this->ntrain; ++i)
+            {
+                ifs >> this->validation_a[i];
+            }
+        }
+        else if (strcmp("loss", word) == 0)
+        {
+            this->read_value(ifs, this->loss);
+        }
+        else if (strcmp("nepoch", word) == 0)
+        {
+            this->read_value(ifs, this->nepoch);
+        }
+        else if (strcmp("step_length", word) == 0)
+        {
+            this->read_value(ifs, this->step_length);
+        }
+        else if (strcmp("dump_fre", word) == 0)
+        {
+            this->read_value(ifs, this->dump_fre);
+        }
+        else if (strcmp("print_fre", word) == 0)
+        {
+            this->read_value(ifs, this->print_fre);
+        }
+        else if (strcmp("gamma", word) == 0)
+        {
+            this->read_value(ifs, this->ml_gamma);
+        }
+        else if (strcmp("p", word) == 0)
+        {
+            this->read_value(ifs, this->ml_p);
+        }
+        else if (strcmp("q", word) == 0)
+        {
+            this->read_value(ifs, this->ml_q);
+        }
+        else if (strcmp("gammanl", word) == 0)
+        {
+            this->read_value(ifs, this->ml_gammanl);
+        }
+        else if (strcmp("pnl", word) == 0)
+        {
+            this->read_value(ifs, this->ml_pnl);
+        }
+        else if (strcmp("qnl", word) == 0)
+        {
+            this->read_value(ifs, this->ml_qnl);
+        }
+    }
+
+    std::cout << "Read nnINPUT done" << std::endl;
+
+    this->nx = this->fftdim * this->fftdim * this->fftdim * this->ntrain;
+    this->nx_vali = this->fftdim * this->fftdim * this->fftdim * this->nvalidation;
+    this->nn_input_index = {{"gamma", -1}, {"p", -1}, {"q", -1}, {"gammanl", -1}, {"pnl", -1}, {"qnl", -1}};
+
+    this->ninput = 0;
+
+    this->rho = std::vector<double>(this->nx);
+    if (this->nvalidation > 0) this->rho_vali = std::vector<double>(this->nx_vali);
+
+    if (this->ml_gamma || this->ml_gammanl){
+        this->gamma = std::vector<double>(this->nx);
+        if (this->nvalidation > 0) this->gamma_vali = std::vector<double>(this->nx_vali);
+        if (this->ml_gamma)
+        {
+            this->nn_input_index["gamma"] = this->ninput; 
+            this->ninput++;
+        } 
+    }    
+    if (this->ml_p || this->ml_pnl){
+        this->p = std::vector<double>(this->nx);
+        this->nablaRho = std::vector<std::vector<double> >(3, std::vector<double>(this->nx, 0.));
+        if (this->nvalidation > 0) this->p_vali = std::vector<double>(this->nx_vali);
+        if (this->nvalidation > 0) this->nablaRho_vali = std::vector<std::vector<double> >(3, std::vector<double>(this->nx_vali, 0.));
+        if (this->ml_p)
+        {
+            this->nn_input_index["p"] = this->ninput;
+            this->ninput++;
+        }
+    }
+    if (this->ml_q || this->ml_qnl){
+        this->q = std::vector<double>(this->nx);
+        if (this->nvalidation > 0) this->q_vali = std::vector<double>(this->nx_vali);
+        if (this->ml_q)
+        {
+            this->nn_input_index["q"] = this->ninput;
+            this->ninput++;
+        }
+    }
+    if (this->ml_gammanl){
+        this->gammanl = std::vector<double>(this->nx);
+        if (this->nvalidation > 0) this->gammanl_vali = std::vector<double>(this->nx_vali);
+        this->nn_input_index["gammanl"] = this->ninput;
+        this->ninput++;
+    }
+    if (this->ml_pnl){
+        this->pnl = std::vector<double>(this->nx);
+        if (this->nvalidation > 0) this->pnl_vali = std::vector<double>(this->nx_vali);
+        this->nn_input_index["pnl"] = this->ninput;
+        this->ninput++;
+    }
+    if (this->ml_qnl){
+        this->qnl = std::vector<double>(this->nx);
+        if (this->nvalidation > 0) this->qnl_vali = std::vector<double>(this->nx_vali);
+        this->nn_input_index["qnl"] = this->ninput;
+        this->ninput++;
+    }
+    std::cout << "ninput" << this->ninput << std::endl;
+}
 
 void Train::loadData()
 {
-    std::vector<long unsigned int> cshape = {(long unsigned) this->nrxx};
+    this->loadData(this->train_dir, this->nx, this->rho, this->gamma, this->p, this->q, this->gammanl, this->pnl, this->qnl, this->nablaRho, this->enhancement);
+    if (this->nvalidation > 0)
+    {
+        this->loadData(this->validation_dir, this->nx_vali, this->rho_vali, this->gamma_vali, this->p_vali, this->q_vali,
+                        this->gammanl_vali, this->pnl_vali, this->qnl_vali, this->nablaRho_vali, this->enhancement_vali);
+        this->input_vali = torch::zeros({this->nx_vali, this->ninput});
+        if (this->nn_input_index["gamma"] >= 0) this->input_vali.index({"...", this->nn_input_index["gamma"]}) = torch::tensor(gamma_vali);
+        if (this->nn_input_index["p"] >= 0) this->input_vali.index({"...", this->nn_input_index["p"]}) = torch::tensor(p_vali);
+        if (this->nn_input_index["q"] >= 0) this->input_vali.index({"...", this->nn_input_index["q"]}) = torch::tensor(q_vali);
+        if (this->nn_input_index["gammanl"] >= 0) this->input_vali.index({"...", this->nn_input_index["gammanl"]}) = torch::tensor(gammanl_vali);
+        if (this->nn_input_index["pnl"] >= 0) this->input_vali.index({"...", this->nn_input_index["pnl"]}) = torch::tensor(pnl_vali);
+        if (this->nn_input_index["qnl"] >= 0) this->input_vali.index({"...", this->nn_input_index["qnl"]}) = torch::tensor(qnl_vali);
+    }
+
+    std::cout << "Load train set done" << std::endl;
+}
+
+void Train::loadData(
+    std::string dir, 
+    int nx,
+    std::vector<double> &rho,
+    std::vector<double> &gamma,
+    std::vector<double> &p,
+    std::vector<double> &q,
+    std::vector<double> &gammanl,
+    std::vector<double> &pnl,
+    std::vector<double> &qnl,
+    std::vector<std::vector<double> > &nablaRho,
+    torch::Tensor &enhancement
+)
+{
+    std::vector<long unsigned int> cshape = {(long unsigned) nx};
     bool fortran_order = false;
-    npy::LoadArrayFromNumpy("gamma.npy", cshape, fortran_order, this->gamma);
-    // npy::LoadArrayFromNumpy("gammanl.npy", cshape, fortran_order, this->gammanl);
-    npy::LoadArrayFromNumpy("p.npy", cshape, fortran_order, this->p);
-    // npy::LoadArrayFromNumpy("pnl.npy", cshape, fortran_order, this->pnl);
-    npy::LoadArrayFromNumpy("q.npy", cshape, fortran_order, this->q);
-    // npy::LoadArrayFromNumpy("qnl.npy", cshape, fortran_order, this->qnl);
+    npy::LoadArrayFromNumpy(dir+"/gamma.npy", cshape, fortran_order, rho);
+    if (this->ml_gamma || this->ml_gammanl) npy::LoadArrayFromNumpy(dir + "/gamma.npy", cshape, fortran_order, gamma);
+    if (this->ml_gammanl) npy::LoadArrayFromNumpy(dir + "/gammanl.npy", cshape, fortran_order, gammanl);
+    if (this->ml_p || this->ml_pnl) npy::LoadArrayFromNumpy(dir + "/p.npy", cshape, fortran_order, p);
+    if (this->ml_pnl) npy::LoadArrayFromNumpy(dir + "/pnl.npy", cshape, fortran_order, pnl);
+    if (this->ml_q || this->ml_qnl) npy::LoadArrayFromNumpy(dir + "/q.npy", cshape, fortran_order, q);
+    if (this->ml_qnl) npy::LoadArrayFromNumpy(dir + "/qnl.npy", cshape, fortran_order, qnl);
 
     std::vector<double> enhancement_;
-    npy::LoadArrayFromNumpy("enhancement.npy", cshape, fortran_order, enhancement_);
-    this->enhancement = torch::tensor(enhancement_);
-    this->enhancement.resize_({this->nrxx, 1});
-
-    std::cout << "load data done" << std::endl;
+    npy::LoadArrayFromNumpy(dir+"enhancement.npy", cshape, fortran_order, enhancement_);
+    enhancement = torch::tensor(enhancement_);
+    enhancement.resize_({nx, 1});
 }
 
 void Train::initNN()
 {
-    this->nn->setData(this->gamma, this->gammanl, this->p, this->pnl, this->q, this->qnl);
+    this->nn = std::make_shared<NN_OFImpl>(this->nx, this->ninput);
+    this->nn->setData(this->nn_input_index, this->gamma, this->gammanl, this->p, this->pnl, this->q, this->qnl);
 }
 
 torch::Tensor Train::lossFunction(torch::Tensor enhancement, torch::Tensor target)
@@ -52,42 +256,40 @@ torch::Tensor Train::lossFunction(torch::Tensor enhancement, torch::Tensor targe
 
 void Train::train()
 {
-
-    std::cout << "train begin" << std::endl;
+    std::cout << "Train begin" << std::endl;
     auto dataset = OF_data(this->nn->inputs, this->enhancement).map(torch::data::transforms::Stack<>());
     auto data_loader = torch::data::make_data_loader(dataset, this->nbatch);
 
-    // std::cout << "size" << dataset.size() << std::endl;
+    torch::optim::SGD optimizer(this->nn->parameters(), this->step_length);
+    std::cout << "Epoch\tLoss\tValidation\n";
+    double lossTrain = 0.;
+    double lossVali = 0.;
 
-    torch::optim::SGD optimizer(this->nn->parameters(), 0.01);
-    // std::cout << this->nn->parameters() << std::endl;
-    std::cout << "Epoch\tLoss\n";
-    for (size_t epoch = 1; epoch <= 10000; ++epoch)
+    for (size_t epoch = 1; epoch <= this->nepoch; ++epoch)
     {
         size_t batch_index = 0;
         for (auto& batch : *data_loader)
         {
-            // std::cout << "inpt" <<batch.data << std::endl;
-            // std::cout << "target" << batch.target << std::endl;
             optimizer.zero_grad();
             torch::Tensor prediction = this->nn->forward(batch.data);
-            // std::cout << "prediction" << prediction << std::endl;
             torch::Tensor loss = this->lossFunction(prediction, batch.target);
-            // torch::Tensor loss = torch::mse_loss(prediction, batch.target);
-            // std::cout << "loss" << loss << std::endl;
-            // std::cout << "batch_index" << batch_index << std::endl;
+            lossTrain = loss.item<double>();
             loss.backward();
-            // break;
             optimizer.step();
-            if (++batch_index % (27*27) == 0) {
-                std::stringstream file;
-                file << "model/net" << epoch << ".pt";
-                std::cout << epoch << "\t" << loss.item<double>() << std::endl;
-                // std::cout << "Epoch: " << epoch << " | Batch: " << batch_index
-                //         << " | Loss: " << loss << std::endl;
-                // Serialize your model periodically as a checkpoint.
-                torch::save(this->nn, file.str());
+        }
+        if (epoch % this->dump_fre == 0)
+        {
+            std::stringstream file;
+            file << "model/net" << epoch << ".pt";
+            torch::save(this->nn, file.str());
+        }
+        if (epoch % this->print_fre == 0) {
+            if (this->nvalidation > 0)
+            {
+                torch::Tensor valid_pre = this->nn->forward(this->input_vali);
+                lossVali = this->lossFunction(valid_pre, this->enhancement_vali).item<double>();
             }
+            std::cout << std::setiosflags(std::ios::scientific) << std::setprecision(3) << epoch << std::setw(12) << lossTrain <<  std::setw(12) << lossVali << std::endl;
         }
         // break;
     }
@@ -96,8 +298,8 @@ void Train::train()
 int main()
 {
     // train test
-    Train train(19683, 3, 27);
-    // Train train(19683, 3, 19683);
+    Train train;
+    train.readInput();
     train.loadData();
     train.initNN();
     train.train();
--- source/module_esolver/ml-of/train.h
+++ source/module_esolver/ml-of/train.h
@@ -4,12 +4,14 @@
 
 class Train{
 public:
-    Train(int nrxx, int ninpt, int nbatch);
+    Train(){};
     ~Train();
     
     std::shared_ptr<NN_OFImpl> nn;
 
-    // void setPara(int nrxx, int ninpt, int nbatch);
+    // void setPara(int nx, int ninpt, int nbatch);
+
+    void readInput();
 
     void loadData();
 
@@ -22,22 +24,86 @@ public:
 
     void dump();
 
-    int nrxx = 1;
-    int ninpt = 6;
-    int nbatch = 10;
+    int nx = 1;
+    int nx_vali = 1;
+    int ninput = 6;
 
+    // training set =============================
     std::vector<double> rho;
     // inputs
     std::vector<double> gamma;
-    std::vector<double> gammanl;
     std::vector<double> p;
-    std::vector<double> pnl;
     std::vector<double> q;
+    std::vector<double> gammanl;
+    std::vector<double> pnl;
     std::vector<double> qnl;
-
+    std::vector<std::vector<double> > nablaRho;
     // target
-    // std::vector<double> enhancement;
     torch::Tensor enhancement;
+    // ==========================================
+
+    // validation set ===========================
+    std::vector<double> rho_vali;
+    // inputs
+    std::vector<double> gamma_vali;
+    std::vector<double> p_vali;
+    std::vector<double> q_vali;
+    std::vector<double> gammanl_vali;
+    std::vector<double> pnl_vali;
+    std::vector<double> qnl_vali;
+    std::vector<std::vector<double> > nablaRho_vali;
+    // target
+    torch::Tensor enhancement_vali;
+    torch::Tensor input_vali;
+    // ==========================================
+
+private:
+    template <class T>
+    static void read_value(std::ifstream &ifs, T &var)
+    {
+        ifs >> var;
+        ifs.ignore(150, '\n');
+        return;
+    }
+
+    void loadData(
+    std::string dir, 
+    int nx,
+    std::vector<double> &rho,
+    std::vector<double> &gamma,
+    std::vector<double> &p,
+    std::vector<double> &q,
+    std::vector<double> &gammanl,
+    std::vector<double> &pnl,
+    std::vector<double> &qnl,
+    std::vector<std::vector<double> > &nablaRho,
+    torch::Tensor &enhancement
+    );
+
+    // input variables
+    int fftdim = 0;
+    int nbatch = 0;
+    int ntrain = 1;
+    int nvalidation = 0;
+    std::string train_dir = ".";
+    std::string *train_cell = nullptr;
+    double *train_a = nullptr;
+    std::string validation_dir = ".";
+    std::string *validation_cell = nullptr;
+    double *validation_a = nullptr;
+    std::string loss = "energy";
+    int nepoch = 1000;
+    double step_length = 0.01;
+    int dump_fre = 1;
+    int print_fre = 1;
+    bool ml_gamma = false;
+    bool ml_p = false;
+    bool ml_q = false;
+    bool ml_gammanl = false;
+    bool ml_pnl = false;
+    bool ml_qnl = false;
+
+    std::map<std::string, int> nn_input_index;
 };
 
 class OF_data : public torch::data::Dataset<OF_data>
