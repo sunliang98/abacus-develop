bd19b4804f4b63e70acdee1cbed145458582b68d
Erjie Wu <110683255+ErjieWu@users.noreply.github.com>
2025-12-23 16:35:41 +0800

    Fix indptr overflow. (#6803)

--- source/source_io/single_R_io.cpp
+++ source/source_io/single_R_io.cpp
@@ -6,12 +6,11 @@
 
 inline void write_data(std::ofstream& ofs, const double& data)
 {
-    ofs << " " << std::fixed << std::scientific << std::setprecision(8) << data;
+    ofs << " " << data;
 }
 inline void write_data(std::ofstream& ofs, const std::complex<double>& data)
 {
-    ofs << " (" << std::fixed << std::scientific << std::setprecision(8) << data.real() << ","
-        << std::fixed << std::scientific << std::setprecision(8) << data.imag() << ")";
+    ofs << " (" << data.real() << "," << data.imag() << ")";
 }
 
 template<typename T>
@@ -68,7 +67,8 @@ void ModuleIO::output_single_R(std::ofstream& ofs,
 
         if (!reduce || GlobalV::DRANK == 0)
         {
-            int nonzeros_count = 0;
+            long long nonzeros_count = 0;
+            ofs << std::fixed << std::scientific << std::setprecision(8);
             for (int col = 0; col < PARAM.globalv.nlocal; ++col)
             {
                 if (std::abs(line[col]) > sparse_threshold)
@@ -106,7 +106,7 @@ void ModuleIO::output_single_R(std::ofstream& ofs,
             ifs_tem1.close();
             for (auto &i : indptr)
             {
-                ofs.write(reinterpret_cast<char *>(&i), sizeof(int));
+                ofs.write(reinterpret_cast<char *>(&i), sizeof(long long));
             }
         }
         else
