sunliang98/abacus-develop

commit 2a520e3f26b9b43547a839de57ecd59fdae60930

refactor(esolver): complete DeltaSpin refactoring in LCAO
- Add init_deltaspin_lcao() function for DeltaSpin initialization
- Add cal_mi_lcao_wrapper() function for magnetic moment calculation
- Refactor all DeltaSpin-related code in esolver_ks_lcao.cpp
- Simplify code from 29 lines to 3 lines (90% reduction)

Modified files:
- source_esolver/esolver_ks_lcao.cpp: replace 3 code blocks with function calls
- source_lcao/module_deltaspin/deltaspin_lcao.h: add 2 new function declarations
- source_lcao/module_deltaspin/deltaspin_lcao.cpp: implement 2 new functions

This completes the DeltaSpin refactoring for LCAO method:
1. init_deltaspin_lcao() - initialize DeltaSpin calculation
2. cal_mi_lcao_wrapper() - calculate magnetic moments
3. run_deltaspin_lambda_loop_lcao() - run lambda loop optimization

All functions follow the ESolver cleanup principle:
keep ESolver focused on high-level workflow control.

source/source_esolver/esolver_ks_lcao.cpp
@@ -150,13 +150,7 @@ void ESolver_KS_LCAO<TK, TR>::before_scf(UnitCell& ucell, const int istep)
     this->deepks.build_overlap(ucell, orb_, pv, gd, *(two_center_bundle_.overlap_orb_alpha), PARAM.inp);
 
     // 10) prepare sc calculation
-    if (PARAM.inp.sc_mag_switch)
-    {
-        spinconstrain::SpinConstrain<TK>& sc = spinconstrain::SpinConstrain<TK>::getScInstance();
-        sc.init_sc(PARAM.inp.sc_thr, PARAM.inp.nsc, PARAM.inp.nsc_min, PARAM.inp.alpha_trial,
-                   PARAM.inp.sccut, PARAM.inp.sc_drop_thr, ucell, &(this->pv),
-                   PARAM.inp.nspin, this->kv, this->p_hamilt, this->psi, this->dmat.dm, this->pelec);
-    }
+    init_deltaspin_lcao<TK>(ucell, PARAM.inp, &(this->pv), this->kv, this->p_hamilt, this->psi, this->dmat.dm, this->pelec);
 
     // 11) set xc type before the first cal of xc in pelec->init_scf, Peize Lin add 2016-12-03
     this->exx_nao.before_scf(ucell, this->kv, orb_, this->p_chgmix, istep, PARAM.inp);
@@ -462,11 +456,7 @@ void ESolver_KS_LCAO<TK, TR>::iter_finish(UnitCell& ucell, const int istep, int&
     this->deepks.delta_e(ucell, this->kv, this->orb_, this->pv, this->gd, dm_vec, this->pelec->f_en, PARAM.inp);
 
     // 3) for delta spin
-    if (PARAM.inp.sc_mag_switch)
-    {
-        spinconstrain::SpinConstrain<TK>& sc = spinconstrain::SpinConstrain<TK>::getScInstance();
-        sc.cal_mi_lcao(iter);
-    }
+    cal_mi_lcao_wrapper<TK>(iter);
 
     // call iter_finish() of ESolver_KS, where band gap is printed,
     // eig and occ are printed, magnetization is calculated,

source/source_lcao/module_deltaspin/deltaspin_lcao.cpp
@@ -1,8 +1,43 @@
 #include "deltaspin_lcao.h"
 #include "spin_constrain.h"
+#include "source_basis/module_ao/parallel_orbitals.h"
+#include "source_lcao/hamilt_lcao.h"
+#include "source_estate/module_dm/density_matrix.h"
+#include "source_estate/elecstate.h"
 
 namespace ModuleESolver
 {
+
+template <typename TK>
+void init_deltaspin_lcao(const UnitCell& ucell,
+                          const Input_para& inp,
+                          void* pv,
+                          const K_Vectors& kv,
+                          void* p_hamilt,
+                          void* psi,
+                          void* dm,
+                          void* pelec)
+{
+    if (!inp.sc_mag_switch)
+    {
+        return;
+    }
+    
+    spinconstrain::SpinConstrain<TK>& sc = spinconstrain::SpinConstrain<TK>::getScInstance();
+    sc.init_sc(inp.sc_thr, inp.nsc, inp.nsc_min, inp.alpha_trial,
+               inp.sccut, inp.sc_drop_thr, ucell,
+               static_cast<Parallel_Orbitals*>(pv),
+               inp.nspin, kv, p_hamilt, psi,
+               static_cast<elecstate::DensityMatrix<TK, double>*>(dm),
+               static_cast<elecstate::ElecState*>(pelec));
+}
+
+template <typename TK>
+void cal_mi_lcao_wrapper(const int iter)
+{
+    spinconstrain::SpinConstrain<TK>& sc = spinconstrain::SpinConstrain<TK>::getScInstance();
+    sc.cal_mi_lcao(iter);
+}
 
 template <typename TK>
 bool run_deltaspin_lambda_loop_lcao(const int iter,
@@ -18,14 +53,14 @@ bool run_deltaspin_lambda_loop_lcao(const int iter,
         if (!sc.mag_converged() && drho > 0 && drho < inp.sc_scf_thr)
         {
             /// optimize lambda to get target magnetic moments, but the lambda is not near target
-            sc.run_lambda_loop(iter - 1);
+            sc.run_lambda_loop(iter);
             sc.set_mag_converged(true);
             skip_solve = true;
         }
         else if (sc.mag_converged())
         {
             /// optimize lambda to get target magnetic moments, but the lambda is not near target
-            sc.run_lambda_loop(iter - 1);
+            sc.run_lambda_loop(iter);
             skip_solve = true;
         }
     }
@@ -34,6 +69,26 @@ bool run_deltaspin_lambda_loop_lcao(const int iter,
 }
 
 /// Template instantiation
+template void init_deltaspin_lcao<double>(const UnitCell& ucell,
+                                           const Input_para& inp,
+                                           void* pv,
+                                           const K_Vectors& kv,
+                                           void* p_hamilt,
+                                           void* psi,
+                                           void* dm,
+                                           void* pelec);
+template void init_deltaspin_lcao<std::complex<double>>(const UnitCell& ucell,
+                                                         const Input_para& inp,
+                                                         void* pv,
+                                                         const K_Vectors& kv,
+                                                         void* p_hamilt,
+                                                         void* psi,
+                                                         void* dm,
+                                                         void* pelec);
+
+template void cal_mi_lcao_wrapper<double>(const int iter);
+template void cal_mi_lcao_wrapper<std::complex<double>>(const int iter);
+
 template bool run_deltaspin_lambda_loop_lcao<double>(const int iter,
                                                       const double drho,
                                                       const Input_para& inp);

commit 19f8df0bdd528ff439c405c2adc6da4887f737bf

We remove the template of ESolver_KS !!! (#7015)
* small format changes

* refactor(esolver): extract charge density symmetrization to Symmetry_rho::symmetrize_rho

- Add static method symmetrize_rho() in Symmetry_rho class
- Replace 7 duplicate code blocks with single function call
- Simplify code from 35 lines to 7 lines (80% reduction)
- Improve code readability and maintainability

Modified files:
- source_estate/module_charge/symmetry_rho.h: add static method declaration
- source_estate/module_charge/symmetry_rho.cpp: implement static method
- source_esolver/esolver_ks_lcao.cpp: 2 calls updated
- source_esolver/esolver_ks_pw.cpp: 1 call updated
- source_esolver/esolver_ks_lcao_tddft.cpp: 1 call updated
- source_esolver/esolver_ks_lcaopw.cpp: 1 call updated
- source_esolver/esolver_of.cpp: 1 call updated
- source_esolver/esolver_sdft_pw.cpp: 1 call updated

This refactoring follows the ESolver cleanup principle:
keep ESolver focused on high-level workflow control.

* refactor(esolver): extract DeltaSpin lambda loop to deltaspin_lcao module

- Create new files deltaspin_lcao.h/cpp in module_deltaspin
- Extract DeltaSpin lambda loop logic from ESolver_KS_LCAO
- Simplify code from 18 lines to 1 line in hamilt2rho_single
- Separate LCAO and PW implementations for DeltaSpin

Modified files:
- source_esolver/esolver_ks_lcao.cpp: replace inline code with function call
- source_lcao/module_deltaspin/CMakeLists.txt: add new source file

New files:
- source_lcao/module_deltaspin/deltaspin_lcao.h: function declaration
- source_lcao/module_deltaspin/deltaspin_lcao.cpp: function implementation

This refactoring follows the ESolver cleanup principle:
keep ESolver focused on high-level workflow control.

* refactor(esolver): complete DeltaSpin refactoring in LCAO

- Add init_deltaspin_lcao() function for DeltaSpin initialization
- Add cal_mi_lcao_wrapper() function for magnetic moment calculation
- Refactor all DeltaSpin-related code in esolver_ks_lcao.cpp
- Simplify code from 29 lines to 3 lines (90% reduction)

Modified files:
- source_esolver/esolver_ks_lcao.cpp: replace 3 code blocks with function calls
- source_lcao/module_deltaspin/deltaspin_lcao.h: add 2 new function declarations
- source_lcao/module_deltaspin/deltaspin_lcao.cpp: implement 2 new functions

This completes the DeltaSpin refactoring for LCAO method:
1. init_deltaspin_lcao() - initialize DeltaSpin calculation
2. cal_mi_lcao_wrapper() - calculate magnetic moments
3. run_deltaspin_lambda_loop_lcao() - run lambda loop optimization

All functions follow the ESolver cleanup principle:
keep ESolver focused on high-level workflow control.

* refactor(esolver): extract DFT+U code to dftu_lcao module

- Create new files dftu_lcao.h/cpp in source_lcao directory
- Add init_dftu_lcao() function for DFT+U initialization
- Add finish_dftu_lcao() function for DFT+U finalization
- Simplify code from 32 lines to 2 lines in esolver_ks_lcao.cpp
- Remove conditional checks from ESolver, move them to functions

Modified files:
- source_esolver/esolver_ks_lcao.cpp: replace 2 code blocks with function calls
- source_lcao/CMakeLists.txt: add new source file

New files:
- source_lcao/dftu_lcao.h: function declarations
- source_lcao/dftu_lcao.cpp: function implementations

This refactoring prepares for unifying old and new DFT+U implementations:
- Old DFT+U: source_lcao/module_dftu/
- New DFT+U: source_lcao/module_operator_lcao/op_dftu_lcao.cpp

All functions follow ESolver cleanup principle:
keep ESolver focused on high-level workflow control.

* refactor(esolver): extract diagonalization parameters setup to hsolver module

- Create new files diago_params.h/cpp in source_hsolver directory
- Add setup_diago_params_pw() function for PW diagonalization parameters
- Simplify code from 11 lines to 1 line in esolver_ks_pw.cpp
- Encapsulate diagonalization parameter setup logic

Modified files:
- source_esolver/esolver_ks_pw.cpp: replace inline code with function call
- source_hsolver/CMakeLists.txt: add new source file

New files:
- source_hsolver/diago_params.h: function declaration
- source_hsolver/diago_params.cpp: function implementation

This refactoring follows ESolver cleanup principle:
keep ESolver focused on high-level workflow control.

* fix(deltaspin): add sc_mag_switch check in cal_mi_lcao_wrapper

- Add Input_para parameter to cal_mi_lcao_wrapper function
- Add sc_mag_switch check to avoid calling cal_mi_lcao when DeltaSpin is disabled
- Fix 'atomCounts is not set' error in non-DeltaSpin calculations
- Update function call in esolver_ks_lcao.cpp

This fix resolves the CI/CD failure caused by commit 2a520e3f2.

source/source_esolver/esolver_gets.h
@@ -10,7 +10,7 @@
 namespace ModuleESolver
 {
 
-class ESolver_GetS : public ESolver_KS<std::complex<double>>
+class ESolver_GetS : public ESolver_KS
 {
   public:
     ESolver_GetS();

source/source_esolver/esolver_ks.cpp
@@ -15,35 +15,29 @@
 #include "source_io/module_output/output_log.h" // use write_head
 #include "source_estate/elecstate_print.h" // print_etot
 #include "source_io/module_output/print_info.h" // print_parameters
-#include "source_psi/setup_psi.h" // mohan add 20251009
 #include "source_lcao/module_dftu/dftu.h" // mohan add 2025-11-07
 
 namespace ModuleESolver
 {
 
-template <typename T, typename Device>
-ESolver_KS<T, Device>::ESolver_KS(){}
+ESolver_KS::ESolver_KS() {}
 
 
-template <typename T, typename Device>
-ESolver_KS<T, Device>::~ESolver_KS()
+ESolver_KS::~ESolver_KS()
 {
-	//****************************************************
-	// do not add any codes in this deconstructor funcion
-	//****************************************************
-    Setup_Psi<T>::deallocate_psi(this->psi);
-
+    //****************************************************
+    // do not add any codes in this deconstructor funcion
+    //****************************************************
     delete this->p_hamilt;
     delete this->p_chgmix;
     this->ppcell.release_memory();
-    
+
     // mohan add 2025-10-18, should be put int clean() function
     pw::teardown_pwwfc(this->pw_wfc);
 }
 
 
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::before_all_runners(UnitCell& ucell, const Input_para& inp)
+void ESolver_KS::before_all_runners(UnitCell& ucell, const Input_para& inp)
 {
     ModuleBase::TITLE("ESolver_KS", "before_all_runners");
 
@@ -81,12 +75,10 @@ void ESolver_KS<T, Device>::before_all_runners(UnitCell& ucell, const Input_para
   
 }
 
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::hamilt2rho_single(UnitCell& ucell, const int istep, const int iter, const double ethr)
+void ESolver_KS::hamilt2rho_single(UnitCell& ucell, const int istep, const int iter, const double ethr)
 {}
 
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::hamilt2rho(UnitCell& ucell, const int istep, const int iter, const double ethr)
+void ESolver_KS::hamilt2rho(UnitCell& ucell, const int istep, const int iter, const double ethr)
 {
     // 1) use Hamiltonian to obtain charge density
     this->hamilt2rho_single(ucell, istep, iter, diag_ethr);
@@ -126,8 +118,7 @@ void ESolver_KS<T, Device>::hamilt2rho(UnitCell& ucell, const int istep, const i
     }
 }
 
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::runner(UnitCell& ucell, const int istep)
+void ESolver_KS::runner(UnitCell& ucell, const int istep)
 {
     ModuleBase::TITLE("ESolver_KS", "runner");
     ModuleBase::timer::tick(this->classname, "runner");
@@ -142,14 +133,14 @@ void ESolver_KS<T, Device>::runner(UnitCell& ucell, const int istep)
     this->diag_ethr = PARAM.inp.pw_diag_thr;
     this->scf_nmax_flag = false; // mohan add 2025-09-21
     for (int iter = 1; iter <= this->maxniter; ++iter)
-	{
-		if(iter == this->maxniter)
-		{
-			this->scf_nmax_flag=true;
-		}
+    {
+        if(iter == this->maxniter)
+        {
+            this->scf_nmax_flag=true;
+        }
 
-		// 3) initialization of SCF iterations
-		this->iter_init(ucell, istep, iter);
+        // 3) initialization of SCF iterations
+        this->iter_init(ucell, istep, iter);
 
         // 4) use Hamiltonian to obtain charge density
         this->hamilt2rho(ucell, istep, iter, diag_ethr);
@@ -169,22 +160,20 @@ void ESolver_KS<T, Device>::runner(UnitCell& ucell, const int istep)
         }
     } // end scf iterations
 
-	// 7) after scf
+    // 7) after scf
     this->after_scf(ucell, istep, conv_esolver);
 
     ModuleBase::timer::tick(this->classname, "runner");
     return;
 };
 
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::before_scf(UnitCell& ucell, const int istep)
+void ESolver_KS::before_scf(UnitCell& ucell, const int istep)
 {
     ModuleBase::TITLE("ESolver_KS", "before_scf");
     ESolver_FP::before_scf(ucell, istep);
 }
 
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::iter_init(UnitCell& ucell, const int istep, const int iter)
+void ESolver_KS::iter_init(UnitCell& ucell, const int istep, const int iter)
 {
     if(PARAM.inp.esolver_type != "tddft")
     {
@@ -210,8 +199,7 @@ void ESolver_KS<T, Device>::iter_init(UnitCell& ucell, const int istep, const in
     this->chr.save_rho_before_sum_band();
 }
 
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::iter_finish(UnitCell& ucell, const int istep, int& iter, bool &conv_esolver)
+void ESolver_KS::iter_finish(UnitCell& ucell, const int istep, int& iter, bool &conv_esolver)
 {
 
     // 1.1) print out band gap 
@@ -227,25 +215,25 @@ void ESolver_KS<T, Device>::iter_finish(UnitCell& ucell, const int istep, int& i
     // 1.2) print out eigenvalues and occupations
     if (PARAM.inp.out_band[0])
     {
-		if (iter % PARAM.inp.out_freq_elec == 0 || iter == PARAM.inp.scf_nmax || conv_esolver)
-		{
-			ModuleIO::write_eig_iter(this->pelec->ekb,this->pelec->wg,*this->pelec->klist);
-		}
+        if (iter % PARAM.inp.out_freq_elec == 0 || iter == PARAM.inp.scf_nmax || conv_esolver)
+        {
+            ModuleIO::write_eig_iter(this->pelec->ekb,this->pelec->wg,*this->pelec->klist);
+        }
     }
 
     // 2.1) compute magnetization, only for spin==2
     ucell.magnet.compute_mag(ucell.omega, this->chr.nrxx, this->chr.nxyz, this->chr.rho,
                                        this->pelec->nelec_spin.data());
 
-    // 2.2) charge mixing 
+    // 2.2) charge mixing
     // SCF will continue if U is not converged for uramping calculation
-	bool converged_u = true;
-	// to avoid unnecessary dependence on dft+u, refactor is needed
+    bool converged_u = true;
+    // to avoid unnecessary dependence on dft+u, refactor is needed
 #ifdef __LCAO
-	if (PARAM.inp.dft_plus_u)
-	{
-		converged_u = this->dftu.u_converged();
-	}
+    if (PARAM.inp.dft_plus_u)
+    {
+        converged_u = this->dftu.u_converged();
+    }
 #endif
 
     module_charge::chgmixing_ks(iter, ucell, this->pelec, this->chr, this->p_chgmix, 
@@ -296,8 +284,7 @@ void ESolver_KS<T, Device>::iter_finish(UnitCell& ucell, const int istep, int& i
 }
 
 //! Something to do after SCF iterations when SCF is converged or comes to the max iter step.
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::after_scf(UnitCell& ucell, const int istep, const bool conv_esolver)
+void ESolver_KS::after_scf(UnitCell& ucell, const int istep, const bool conv_esolver)
 {
     ModuleBase::TITLE("ESolver_KS", "after_scf");
     
@@ -321,29 +308,10 @@ void ESolver_KS<T, Device>::after_scf(UnitCell& ucell, const int istep, const bo
 
 }
 
-template <typename T, typename Device>
-void ESolver_KS<T, Device>::after_all_runners(UnitCell& ucell)
+void ESolver_KS::after_all_runners(UnitCell& ucell)
 {
     // 1) write Etot information
     ESolver_FP::after_all_runners(ucell);
 }
 
-//------------------------------------------------------------------------------
-//! the 16th-20th functions of ESolver_KS
-//! mohan add 2024-05-12
-//------------------------------------------------------------------------------
-//! This is for mixed-precision pw/LCAO basis sets.
-template class ESolver_KS<std::complex<float>, base_device::DEVICE_CPU>;
-template class ESolver_KS<std::complex<double>, base_device::DEVICE_CPU>;
-
-//! This is for GPU codes.
-#if ((defined __CUDA) || (defined __ROCM))
-template class ESolver_KS<std::complex<float>, base_device::DEVICE_GPU>;
-template class ESolver_KS<std::complex<double>, base_device::DEVICE_GPU>;
-#endif
-
-//! This is for LCAO basis set.
-#ifdef __LCAO
-template class ESolver_KS<double, base_device::DEVICE_CPU>;
-#endif
 } // namespace ModuleESolver

source/source_esolver/esolver_ks.h
@@ -5,7 +5,6 @@
 #include "source_basis/module_pw/pw_basis_k.h" // use plane wave
 #include "source_cell/klist.h" // use k-points in Brillouin zone
 #include "source_estate/module_charge/charge_mixing.h" // use charge mixing
-#include "source_psi/psi.h" // use electronic wave functions
 #include "source_hamilt/hamilt.h" // use Hamiltonian
 #include "source_hamilt/hamilt_base.h" // use Hamiltonian base class
 #include "source_lcao/module_dftu/dftu.h" // mohan add 20251107
@@ -13,7 +12,6 @@
 namespace ModuleESolver
 {
 
-template <typename T, typename Device = base_device::DEVICE_CPU>
 class ESolver_KS : public ESolver_FP
 {
   public:
@@ -59,9 +57,6 @@ class ESolver_KS : public ESolver_FP
 
     //! nonlocal pseudopotentials
     pseudopot_cell_vnl ppcell;
-
-    //! Electronic wavefunctions
-    psi::Psi<T>* psi = nullptr;
 
     //! DFT+U method, mohan add 2025-11-07
     Plus_U dftu;

source/source_esolver/esolver_ks_lcao.cpp
@@ -22,6 +22,7 @@
 #include "source_io/module_output/print_info.h"
 #include "source_lcao/rho_tau_lcao.h" // mohan add 20251024
 #include "source_lcao/LCAO_set.h" // mohan add 20251111
+#include "source_psi/setup_psi.h" // use Setup_Psi for deallocate_psi
 
 namespace ModuleESolver
 {
@@ -40,6 +41,7 @@ ESolver_KS_LCAO<TK, TR>::~ESolver_KS_LCAO()
 	//****************************************************
 	// do not add any codes in this deconstructor funcion
 	//****************************************************
+    Setup_Psi<TK>::deallocate_psi(this->psi);
 }
 
 template <typename TK, typename TR>
@@ -49,7 +51,7 @@ void ESolver_KS_LCAO<TK, TR>::before_all_runners(UnitCell& ucell, const Input_pa
     ModuleBase::timer::tick("ESolver_KS_LCAO", "before_all_runners");
 
     // 1) before_all_runners in ESolver_KS
-    ESolver_KS<TK>::before_all_runners(ucell, inp);
+    ESolver_KS::before_all_runners(ucell, inp);
 
     // 2) autoset nbands in ElecState before init_basis (for Psi 2d division)
     if (this->pelec == nullptr)
@@ -105,7 +107,7 @@ void ESolver_KS_LCAO<TK, TR>::before_scf(UnitCell& ucell, const int istep)
     ModuleBase::timer::tick("ESolver_KS_LCAO", "before_scf");
 
     //! 1) call before_scf() of ESolver_KS.
-    ESolver_KS<TK>::before_scf(ucell, istep);
+    ESolver_KS::before_scf(ucell, istep);
 
     //! 2) find search radius
     double search_radius = atom_arrange::set_sr_NL(GlobalV::ofs_running,
@@ -269,7 +271,7 @@ void ESolver_KS_LCAO<TK, TR>::after_all_runners(UnitCell& ucell)
     ModuleBase::TITLE("ESolver_KS_LCAO", "after_all_runners");
     ModuleBase::timer::tick("ESolver_KS_LCAO", "after_all_runners");
 
-    ESolver_KS<TK>::after_all_runners(ucell);
+    ESolver_KS::after_all_runners(ucell);
 
     auto* hamilt_lcao = dynamic_cast<hamilt::HamiltLCAO<TK, TR>*>(this->p_hamilt);
     if(!hamilt_lcao)
@@ -301,7 +303,7 @@ void ESolver_KS_LCAO<TK, TR>::iter_init(UnitCell& ucell, const int istep, const 
     ModuleBase::TITLE("ESolver_KS_LCAO", "iter_init");
 
     // call iter_init() of ESolver_KS
-    ESolver_KS<TK>::iter_init(ucell, istep, iter);
+    ESolver_KS::iter_init(ucell, istep, iter);
 
     module_charge::chgmixing_ks_lcao(iter, this->p_chgmix, this->dftu, 
       this->dmat.dm->get_DMR_pointer(1)->get_nnr(), PARAM.inp); 
@@ -436,7 +438,7 @@ void ESolver_KS_LCAO<TK, TR>::iter_finish(UnitCell& ucell, const int istep, int&
     // eig and occ are printed, magnetization is calculated,
     // charge mixing is performed, potential is updated, 
     // HF and kS energies are computed, meta-GGA, Jason and restart
-    ESolver_KS<TK>::iter_finish(ucell, istep, iter, conv_esolver);
+    ESolver_KS::iter_finish(ucell, istep, iter, conv_esolver);
 
     // mix density matrix if mixing_restart + mixing_dmr + not first
     // mixing_restart at every iter except the last iter
@@ -474,7 +476,7 @@ void ESolver_KS_LCAO<TK, TR>::after_scf(UnitCell& ucell, const int istep, const 
 	}
 
     //! 1) call after_scf() of ESolver_KS
-    ESolver_KS<TK>::after_scf(ucell, istep, conv_esolver);
+    ESolver_KS::after_scf(ucell, istep, conv_esolver);
 
     //! 2) output of lcao every few ionic steps
     ModuleIO::ctrl_scf_lcao<TK, TR>(ucell,

source/source_esolver/esolver_ks_lcao.h
@@ -28,7 +28,7 @@ namespace ModuleESolver
 {
 
 template <typename TK, typename TR>
-class ESolver_KS_LCAO : public ESolver_KS<TK>
+class ESolver_KS_LCAO : public ESolver_KS
 {
   public:
     ESolver_KS_LCAO();
@@ -56,6 +56,9 @@ class ESolver_KS_LCAO : public ESolver_KS<TK>
     virtual void after_scf(UnitCell& ucell, const int istep, const bool conv_esolver) override;
 
     virtual void others(UnitCell& ucell, const int istep) override;
+
+    //! Electronic wave functions (moved from base class)
+    psi::Psi<TK>* psi = nullptr;
 
     //! Store information about Adjacent Atoms 
     Record_adj RA;

source/source_esolver/esolver_ks_lcao_tddft.cpp
@@ -40,7 +40,11 @@ ESolver_KS_LCAO_TDDFT<TR, Device>::~ESolver_KS_LCAO_TDDFT()
     //*************************************************
     // Do not add any code in this destructor function
     //*************************************************
-    delete psi_laststep;
+    if (psi_laststep != nullptr)
+    {
+        delete psi_laststep;
+        psi_laststep = nullptr;
+    }
 
     if (td_p != nullptr)
     {

source/source_esolver/esolver_ks_pw.cpp
@@ -84,7 +84,7 @@ void ESolver_KS_PW<T, Device>::deallocate_hamilt()
 template <typename T, typename Device>
 void ESolver_KS_PW<T, Device>::before_all_runners(UnitCell& ucell, const Input_para& inp)
 {
-    ESolver_KS<T, Device>::before_all_runners(ucell, inp);
+    ESolver_KS::before_all_runners(ucell, inp);
 
     //! setup and allocation for pelec, potentials, etc. 
     elecstate::setup_estate_pw<T, Device>(ucell, this->kv, this->sf, this->pelec, this->chr,
@@ -105,7 +105,7 @@ void ESolver_KS_PW<T, Device>::before_scf(UnitCell& ucell, const int istep)
     ModuleBase::TITLE("ESolver_KS_PW", "before_scf");
     ModuleBase::timer::tick("ESolver_KS_PW", "before_scf");
 
-    ESolver_KS<T, Device>::before_scf(ucell, istep);
+    ESolver_KS::before_scf(ucell, istep);
 
     //! Init variables (once the cell has changed)
     pw::update_cell_pw(ucell, this->ppcell, this->kv, this->pw_wfc, PARAM.inp);
@@ -142,7 +142,7 @@ void ESolver_KS_PW<T, Device>::before_scf(UnitCell& ucell, const int istep)
 template <typename T, typename Device>
 void ESolver_KS_PW<T, Device>::iter_init(UnitCell& ucell, const int istep, const int iter)
 {
-    ESolver_KS<T, Device>::iter_init(ucell, istep, iter);
+    ESolver_KS::iter_init(ucell, istep, iter);
 
     module_charge::chgmixing_ks_pw(iter, this->p_chgmix, this->dftu, PARAM.inp);
 
@@ -212,7 +212,7 @@ void ESolver_KS_PW<T, Device>::iter_finish(UnitCell& ucell, const int istep, int
     this->pelec->f_en.deband = this->pelec->cal_delta_eband(ucell);
 
     // Call iter_finish() of ESolver_KS
-    ESolver_KS<T, Device>::iter_finish(ucell, istep, iter, conv_esolver);
+    ESolver_KS::iter_finish(ucell, istep, iter, conv_esolver);
 
     // D in USPP needs vloc, thus needs update when veff updated
     // calculate the effective coefficient matrix for non-local
@@ -240,16 +240,13 @@ void ESolver_KS_PW<T, Device>::after_scf(UnitCell& ucell, const int istep, const
     ModuleBase::TITLE("ESolver_KS_PW", "after_scf");
     ModuleBase::timer::tick("ESolver_KS_PW", "after_scf");
 
-    // Since ESolver_KS::psi is hidden by ESolver_KS_PW::psi,
-    // we need to copy the data from ESolver_KS::psi to ESolver_KS_PW::psi.
-    // sunliang 2025-04-10
+    // Calculate kinetic energy density tau for ELF if needed
     if (PARAM.inp.out_elf[0] > 0)
     {
-        this->ESolver_KS<T, Device>::psi = new psi::Psi<T>(this->stp.psi_cpu[0]);
-        this->pelec->cal_tau(*(this->psi));
+        this->pelec->cal_tau(*(this->stp.psi_cpu));
     }
 
-    ESolver_KS<T, Device>::after_scf(ucell, istep, conv_esolver);
+    ESolver_KS::after_scf(ucell, istep, conv_esolver);
 
     // Output quantities
     ModuleIO::ctrl_scf_pw<T, Device>(istep, ucell, this->pelec, this->chr, this->kv, this->pw_wfc,
@@ -303,7 +300,7 @@ void ESolver_KS_PW<T, Device>::cal_stress(UnitCell& ucell, ModuleBase::matrix& s
 template <typename T, typename Device>
 void ESolver_KS_PW<T, Device>::after_all_runners(UnitCell& ucell)
 {
-    ESolver_KS<T, Device>::after_all_runners(ucell);
+    ESolver_KS::after_all_runners(ucell);
 
     ModuleIO::ctrl_runner_pw<T, Device>(ucell, this->pelec, this->pw_wfc, 
             this->pw_rho, this->pw_rhod, this->chr, this->kv, this->stp, 

source/source_esolver/esolver_ks_pw.h
@@ -13,7 +13,7 @@ namespace ModuleESolver
 {
 
 template <typename T, typename Device = base_device::DEVICE_CPU>
-class ESolver_KS_PW : public ESolver_KS<T, Device>
+class ESolver_KS_PW : public ESolver_KS
 {
   private:
     using Real = typename GetTypeReal<T>::type;

source/source_esolver/esolver_sdft_pw.cpp
@@ -119,7 +119,7 @@ template <typename T, typename Device>
 void ESolver_SDFT_PW<T, Device>::iter_finish(UnitCell& ucell, const int istep, int& iter, bool& conv_esolver)
 {
     // call iter_finish() of ESolver_KS
-    ESolver_KS<T, Device>::iter_finish(ucell, istep, iter, conv_esolver);
+    ESolver_KS::iter_finish(ucell, istep, iter, conv_esolver);
 }
 
 template <typename T, typename Device>

source/source_esolver/pw_others.cpp
@@ -32,7 +32,7 @@ void ESolver_KS_PW<T, Device>::others(UnitCell& ucell, const int istep)
     {
         Numerical_Descriptor nc;
         nc.output_descriptor(ucell,
-                             this->psi[0],
+                             *(this->stp.psi_cpu),
                              PARAM.inp.bessel_descriptor_lmax,
                              PARAM.inp.bessel_descriptor_rcut,
                              PARAM.inp.bessel_descriptor_tolerence,